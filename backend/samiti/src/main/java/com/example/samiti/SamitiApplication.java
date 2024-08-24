1648b8bee69364f1184d1d3aac93bcab69a07cbd
Yashx314 <ashuaggarwal842@gmail.com>
2024-08-25 02:51:32 +0530

    initialized backend

---
+++ backend/samiti/src/main/java/com/example/samiti/SamitiApplication.java
@@ -0,0 +1,14 @@
+package com.example.samiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamitiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(SamitiApplication.class, args);
+	}
+
+}
---
+++ backend/samiti/src/test/java/com/example/samiti/SamitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
